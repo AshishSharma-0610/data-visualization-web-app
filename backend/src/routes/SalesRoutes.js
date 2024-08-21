b56ce7baf39b083d200d07aaae8204bd3e5e070c
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-08-21 19:27:35 +0530

    Delete backend/src directory

--- backend/src/routes/SalesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const salesController = require('../controllers/salesController');
-
-router.get('/total', salesController.getTotalSales);
-router.get('/growth-rate', salesController.getSalesGrowthRate);
-
-module.exports = router;
