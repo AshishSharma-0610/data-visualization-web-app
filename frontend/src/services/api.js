55031e6be1bce0ea6424460b8cf44179b2a4879c
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-08-21 20:16:25 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://data-visualization-web-app.onrender.com';
 
 // Fetch total sales over time
 export const getTotalSales = async (interval) => {
