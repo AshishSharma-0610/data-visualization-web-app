3dc18f4ead228e3bdd2fbdea77ef5fa0307eb59f
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-08-21 20:05:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,24 @@ const customerRoutes = require('./routes/customerRoutes');
 
 const app = express();
 
-app.use(cors());
+// Configure CORS to allow only your frontend domain
+const corsOptions = {
+    origin: 'https://data-visualization-web-app-three.vercel.app/', // Replace with your actual Vercel domain
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
+// Connect to the database
 connectDB();
 
+// Register API routes
 app.use('/api/sales', salesRoutes);
 app.use('/api/customers', customerRoutes);
 
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
